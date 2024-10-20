10d6c72f9a43e4c24964d53c8d4fed7be770cdfa
dnexpirydate1801 <dnexpirydate1801@gmail.com>
2024-10-20 08:36:47 +0700

    first commit

---
+++ Assets/Scripts/QuitOnEsc.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class QuitOnEsc : MonoBehaviour
+{
+    void Update()
+    {
+        // Kiểm tra nếu phím ESC được nhấn
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Thoát khỏi ứng dụng
+            Application.Quit();
+
+            // Chỉ dùng cho Editor, giúp dừng chơi khi chạy trong Unity Editor
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
